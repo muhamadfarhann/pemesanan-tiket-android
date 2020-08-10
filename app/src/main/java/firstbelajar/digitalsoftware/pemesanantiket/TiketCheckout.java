24246cdf78ead16dafee4dc50b1b9c42510dd44d
Muhamad Farhan <mfarhan2797@gmail.com>
2020-08-10 13:15:56 +0700

    Penambahan tiket checkout

--- app/src/main/java/firstbelajar/digitalsoftware/pemesanantiket/TiketCheckout.java
+++ app/src/main/java/firstbelajar/digitalsoftware/pemesanantiket/TiketCheckout.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class TiketCheckout extends AppCompatActivity {
 
     TextView nama_wisata, lokasi, ketentuan;
     Button btn_buy_ticket, btnmines, btnpls;
+    LinearLayout btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,15 @@ public class TiketCheckout extends AppCompatActivity {
         final String jenis_tiket_baru = bundle.getString("jenis_tiket");
 
         btn_buy_ticket = findViewById(R.id.btn_buy_ticket);
+        btn_back = findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotodetailtiket = new Intent(TiketCheckout.this, DetailTiket.class);
+                startActivity(gotodetailtiket);
+            }
+        });
 
         btn_buy_ticket.setOnClickListener(new View.OnClickListener() {
             @Override
